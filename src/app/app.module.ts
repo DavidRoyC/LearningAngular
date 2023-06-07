2a8c5c5ea9e69fcac0e9b0e1f4abc58c376a1d8f
David Roy <david.roy.casao@gmail.com>
2023-06-07 19:33:04 +0200

    Importar módulos iniciales en la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MainModule } from './main';
+import { SecurityModule } from './security';
+
+import { ArtAndDocCoreModule } from 'src/lib/art-and-doc-core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MainModule,
+    SecurityModule,
+    AppRoutingModule,
+    ArtAndDocCoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
