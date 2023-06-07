0f7cb3abde199012ab62e124c472288e04383a27
David Roy <david.roy.casao@gmail.com>
2023-06-07 21:11:39 +0200

    Añadir nivel verbosidad a entornos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  LOGGER_VERBOSE_LEVEL: 1, // Solo muestra errores
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 
 export const environment = {
   production: false,
+  LOGGER_VERBOSE_LEVEL: 4, // Muestra todos los niveles de logger
 
 };
