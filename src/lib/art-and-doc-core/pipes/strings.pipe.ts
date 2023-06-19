38b016c3581ba23b622c8909c01f1ddc13e3281d
David Roy <david.roy.casao@gmail.com>
2023-06-19 16:42:58 +0200

    Pipe Mensajes de error

--- src/lib/art-and-doc-core/pipes/strings.pipe.ts
+++ src/lib/art-and-doc-core/pipes/strings.pipe.ts
@@ -1,21 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: 'ellipsis'})
+@Pipe({ name: 'ellipsis' })
 export class EllipsisPipe implements PipeTransform {
   transform(value: string, maxlen: number): string {
-    return (!maxlen || maxlen < 2 || !value || value.length < maxlen) ? value : (value.substr(0, maxlen-1) + '\u2026');
+    return !maxlen || maxlen < 2 || !value || value.length < maxlen
+      ? value
+      : value.substr(0, maxlen - 1) + '\u2026';
   }
 }
 
-@Pipe({ name: 'capitalize'})
+@Pipe({ name: 'capitalize' })
 export class CapitalizePipe implements PipeTransform {
   transform(value: string): string | null {
-    return value?.toString()
+    return value
+      ?.toString()
       .toLowerCase()
       .split('.')
-      .map(str => str.trim())
+      .map((str) => str.trim())
       .map((str, index, array) => {
-        const ending = (array.length > 1 && index + 1 < array.length ? (array[index + 1] === '' ? '.' : '. ') : '');
+        const ending =
+          array.length > 1 && index + 1 < array.length
+            ? array[index + 1] === ''
+              ? '.'
+              : '. '
+            : '';
         str.length === 0
           ? ending
           : str.charAt(0)?.toUpperCase() + str?.substring(1) + ending;
@@ -24,8 +32,49 @@ export class CapitalizePipe implements PipeTransform {
       .trim();
   }
 }
-export const PIPES_STRINGS = [
-  EllipsisPipe,
-  CapitalizePipe,
 
-];
+@Pipe({
+  name: 'error_message',
+})
+export class ErrorMessagePipe implements PipeTransform {
+  transform(value: any): string {
+    if (!value) return '';
+    let msg = '';
+
+    for (const error in value) {
+      switch (error) {
+        case 'required':
+          msg += 'Campo obligatorio.\n';
+          break;
+        case 'minlength':
+          msg += `Debe tener al menos ${value[error].requiredLength} caracteres.\n`;
+          break;
+        case 'maxlength':
+          msg += `Debe tener como máximo ${value[error].requiredLength} caracteres.\n`;
+          break;
+        case 'min':
+          msg += `El valor debe ser mayor o igual que ${value[error].min}.\n`;
+          break;
+        case 'max':
+          msg += `El valor debe ser menor o igual que ${value[error].max}.\n`;
+          break;
+        case 'email':
+        case 'pattern':
+          msg += 'El formato no es correcto.\n';
+          break;
+
+        default:
+          if (typeof value[error] === 'string')
+            msg += `${value[error]}${value[error].endsWith('.') ? '' : '.'}\n`;
+          else if (typeof value[error]?.message === 'string')
+            msg += `${value[error].message}${
+              value[error].message.endsWith('.') ? '' : '.'
+            }\n`;
+          break;
+      }
+    }
+
+    return msg;
+  }
+}
+export const PIPES_STRINGS = [EllipsisPipe, CapitalizePipe, ErrorMessagePipe];
